c750e4d2d1e615a3b341561169f1306f501c191a
pablo-wynistorf <p.wynistorf@gmail.com>
2026-02-02 11:13:57 +0100

    Adding propper build configuration

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,16 +1,33 @@
+############################
+# Build configuration
+############################
+
+locals {
+  build_dir = "${path.module}/.build-lambda-api"
+}
+
+data "aws_caller_identity" "lambda_api" {}
+
+resource "null_resource" "lambda_api_build_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.build_dir}"
+  }
+}
+
+############################
 # Lambda execution role
+############################
+
 resource "aws_iam_role" "lambda" {
   name = "${var.project_name}-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = { Service = "lambda.amazonaws.com" }
-      }
-    ]
+    Statement = [{
+      Effect    = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
   })
 }
 
@@ -22,12 +39,8 @@ resource "aws_iam_role_policy" "lambda" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
+        Effect   = "Allow"
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
         Resource = "arn:aws:logs:*:*:*"
       },
       {
@@ -47,75 +60,105 @@ resource "aws_iam_role_policy" "lambda" {
         ]
       },
       {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject"
-        ]
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject"]
         Resource = [
           "${aws_s3_bucket.files.arn}/*",
           "${aws_s3_bucket.cdn_files.arn}/*"
         ]
       },
       {
-        Effect = "Allow"
-        Action = [
-          "sqs:SendMessage"
-        ]
+        Effect   = "Allow"
+        Action   = ["sqs:SendMessage"]
         Resource = aws_sqs_queue.file_deletion.arn
       }
     ]
   })
 }
 
-# Install npm dependencies for API Lambda
+############################
+# Build & package Lambda
+############################
+
 resource "null_resource" "lambda_api_npm_install" {
   triggers = {
     package_json = filemd5("${path.module}/../src/lambda/api/package.json")
+    package_lock = try(filemd5("${path.module}/../src/lambda/api/package-lock.json"), "")
   }
 
   provisioner "local-exec" {
-    command     = "npm install --production"
+    command     = "npm ci --omit=dev"
     working_dir = "${path.module}/../src/lambda/api"
   }
 }
 
-# Package Lambda code
-data "archive_file" "lambda" {
+data "archive_file" "lambda_api_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../src/lambda/api"
-  output_path = "${path.module}/.terraform/lambda-api.zip"
-  depends_on  = [null_resource.lambda_api_npm_install]
+  output_path = "${local.build_dir}/lambda-api.zip"
+
+  depends_on = [
+    null_resource.lambda_api_build_dir,
+    null_resource.lambda_api_npm_install
+  ]
 }
 
+############################
+# Artifact storage (S3)
+############################
+
+resource "aws_s3_bucket" "lambda_api_artifacts" {
+  bucket = "${var.project_name}-lambda-api-artifacts-${data.aws_caller_identity.lambda_api.account_id}"
+}
+
+resource "aws_s3_object" "lambda_api_object" {
+  bucket       = aws_s3_bucket.lambda_api_artifacts.id
+  key          = "lambda/${data.archive_file.lambda_api_zip.output_base64sha256}.zip"
+  source       = data.archive_file.lambda_api_zip.output_path
+  source_hash  = filemd5(data.archive_file.lambda_api_zip.output_path)
+  content_type = "application/zip"
+}
+
+############################
 # Lambda function
+############################
+
 resource "aws_lambda_function" "api" {
-  filename         = data.archive_file.lambda.output_path
-  function_name    = "${var.project_name}-api"
-  role             = aws_iam_role.lambda.arn
-  handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-  runtime          = "nodejs20.x"
-  timeout          = 30
-  memory_size      = 256
+  function_name = "${var.project_name}-api"
+  role          = aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  timeout       = 30
+  memory_size   = 256
+
+  s3_bucket = aws_s3_object.lambda_api_object.bucket
+  s3_key    = aws_s3_object.lambda_api_object.key
+
+  source_code_hash = data.archive_file.lambda_api_zip.output_base64sha256
 
   environment {
     variables = {
-      BUCKET_NAME            = aws_s3_bucket.files.id
-      CDN_BUCKET_NAME        = aws_s3_bucket.cdn_files.id
-      CDN_URL                = "https://${var.domain_name != "" ? var.domain_name : aws_cloudfront_distribution.main.domain_name}/cdn"
-      FILES_TABLE            = aws_dynamodb_table.files.name
-      SESSIONS_TABLE         = aws_dynamodb_table.sessions.name
-      OIDC_ISSUER            = var.oidc_issuer
-      OIDC_CLIENT_ID         = var.oidc_client_id
-      OIDC_CLIENT_SECRET     = var.oidc_client_secret
-      REDIRECT_URI           = "https://${var.domain_name != "" ? var.domain_name : aws_cloudfront_distribution.main.domain_name}/api/auth/callback"
-      FRONTEND_URL           = "https://${var.domain_name != "" ? var.domain_name : aws_cloudfront_distribution.main.domain_name}"
-      JWT_SECRET             = var.jwt_secret
+      BUCKET_NAME             = aws_s3_bucket.files.id
+      CDN_BUCKET_NAME         = aws_s3_bucket.cdn_files.id
+      CDN_URL                 = "https://${var.domain_name != "" ? var.domain_name : aws_cloudfront_distribution.main.domain_name}/cdn"
+      FILES_TABLE             = aws_dynamodb_table.files.name
+      SESSIONS_TABLE          = aws_dynamodb_table.sessions.name
+      OIDC_ISSUER             = var.oidc_issuer
+      OIDC_CLIENT_ID          = var.oidc_client_id
+      OIDC_CLIENT_SECRET      = var.oidc_client_secret
+      REDIRECT_URI            = "https://${var.domain_name != "" ? var.domain_name : aws_cloudfront_distribution.main.domain_name}/api/auth/callback"
+      FRONTEND_URL            = "https://${var.domain_name != "" ? var.domain_name : aws_cloudfront_distribution.main.domain_name}"
+      JWT_SECRET              = var.jwt_secret
       FILE_DELETION_QUEUE_URL = aws_sqs_queue.file_deletion.url
     }
   }
+
+  depends_on = [aws_s3_object.lambda_api_object]
 }
+
+############################
+# API Gateway permission
+############################
 
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowAPIGatewayInvoke"
--- terraform/lambda_deletion.tf
+++ terraform/lambda_deletion.tf
@@ -1,16 +1,33 @@
-# Lambda execution role for file deletion
+############################
+# Build configuration
+############################
+
+locals {
+  build_dir = "${path.module}/.build-lambda-deletion"
+}
+
+data "aws_caller_identity" "lambda_deletion" {}
+
+resource "null_resource" "lambda_deletion_build_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.build_dir}"
+  }
+}
+
+############################
+# Lambda execution role
+############################
+
 resource "aws_iam_role" "lambda_deletion" {
   name = "${var.project_name}-lambda-deletion-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = { Service = "lambda.amazonaws.com" }
-      }
-    ]
+    Statement = [{
+      Effect    = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
   })
 }
 
@@ -22,111 +39,124 @@ resource "aws_iam_role_policy" "lambda_deletion" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
+        Effect   = "Allow"
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
         Resource = "arn:aws:logs:*:*:*"
       },
       {
-        Effect = "Allow"
-        Action = [
-          "sqs:ReceiveMessage",
-          "sqs:DeleteMessage",
-          "sqs:GetQueueAttributes"
-        ]
+        Effect   = "Allow"
+        Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"]
         Resource = aws_sqs_queue.file_deletion.arn
       },
       {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:DeleteItem"
-        ]
+        Effect   = "Allow"
+        Action   = ["dynamodb:GetItem", "dynamodb:DeleteItem"]
         Resource = aws_dynamodb_table.files.arn
       },
       {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:DescribeStream",
-          "dynamodb:GetRecords",
-          "dynamodb:GetShardIterator",
-          "dynamodb:ListStreams"
-        ]
+        Effect   = "Allow"
+        Action   = ["dynamodb:DescribeStream", "dynamodb:GetRecords", "dynamodb:GetShardIterator", "dynamodb:ListStreams"]
         Resource = "${aws_dynamodb_table.files.arn}/stream/*"
       },
       {
-        Effect = "Allow"
-        Action = [
-          "s3:DeleteObject"
-        ]
+        Effect   = "Allow"
+        Action   = ["s3:DeleteObject"]
         Resource = [
           "${aws_s3_bucket.files.arn}/*",
           "${aws_s3_bucket.cdn_files.arn}/*"
         ]
       },
       {
-        Effect = "Allow"
-        Action = [
-          "cloudfront:CreateInvalidation"
-        ]
+        Effect   = "Allow"
+        Action   = ["cloudfront:CreateInvalidation"]
         Resource = aws_cloudfront_distribution.main.arn
       }
     ]
   })
 }
 
-# Install npm dependencies for deletion Lambda
+############################
+# Build & package Lambda
+############################
+
 resource "null_resource" "lambda_deletion_npm_install" {
   triggers = {
     package_json = filemd5("${path.module}/../src/lambda/deletion/package.json")
+    package_lock = try(filemd5("${path.module}/../src/lambda/deletion/package-lock.json"), "")
   }
 
   provisioner "local-exec" {
-    command     = "npm install --production"
+    command     = "npm ci --omit=dev"
     working_dir = "${path.module}/../src/lambda/deletion"
   }
 }
 
-# Package deletion Lambda code
-data "archive_file" "lambda_deletion" {
+data "archive_file" "lambda_deletion_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../src/lambda/deletion"
-  output_path = "${path.module}/.terraform/lambda-deletion.zip"
-  depends_on  = [null_resource.lambda_deletion_npm_install]
+  output_path = "${local.build_dir}/lambda-deletion.zip"
+
+  depends_on = [
+    null_resource.lambda_deletion_build_dir,
+    null_resource.lambda_deletion_npm_install
+  ]
 }
 
-# File deletion Lambda function
+############################
+# Artifact storage (S3)
+############################
+
+resource "aws_s3_bucket" "lambda_deletion_artifacts" {
+  bucket = "${var.project_name}-lambda-deletion-artifacts-${data.aws_caller_identity.lambda_deletion.account_id}"
+}
+
+resource "aws_s3_object" "lambda_deletion_object" {
+  bucket       = aws_s3_bucket.lambda_deletion_artifacts.id
+  key          = "lambda/${data.archive_file.lambda_deletion_zip.output_base64sha256}.zip"
+  source       = data.archive_file.lambda_deletion_zip.output_path
+  source_hash  = filemd5(data.archive_file.lambda_deletion_zip.output_path)
+  content_type = "application/zip"
+}
+
+############################
+# Lambda function
+############################
+
 resource "aws_lambda_function" "file_deletion" {
-  filename         = data.archive_file.lambda_deletion.output_path
-  function_name    = "${var.project_name}-file-deletion"
-  role             = aws_iam_role.lambda_deletion.arn
-  handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda_deletion.output_base64sha256
-  runtime          = "nodejs20.x"
-  timeout          = 30
-  memory_size      = 128
+  function_name = "${var.project_name}-file-deletion"
+  role          = aws_iam_role.lambda_deletion.arn
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  timeout       = 30
+  memory_size   = 128
+
+  s3_bucket = aws_s3_object.lambda_deletion_object.bucket
+  s3_key    = aws_s3_object.lambda_deletion_object.key
+
+  source_code_hash = data.archive_file.lambda_deletion_zip.output_base64sha256
 
   environment {
     variables = {
-      BUCKET_NAME              = aws_s3_bucket.files.id
-      CDN_BUCKET_NAME          = aws_s3_bucket.cdn_files.id
-      FILES_TABLE              = aws_dynamodb_table.files.name
+      BUCKET_NAME                = aws_s3_bucket.files.id
+      CDN_BUCKET_NAME            = aws_s3_bucket.cdn_files.id
+      FILES_TABLE                = aws_dynamodb_table.files.name
       CLOUDFRONT_DISTRIBUTION_ID = aws_cloudfront_distribution.main.id
     }
   }
+
+  depends_on = [aws_s3_object.lambda_deletion_object]
 }
 
-# DynamoDB Streams event source mapping for TTL-triggered deletions
+############################
+# DynamoDB Stream mapping
+############################
+
 resource "aws_lambda_event_source_mapping" "dynamodb_stream" {
   event_source_arn  = aws_dynamodb_table.files.stream_arn
   function_name     = aws_lambda_function.file_deletion.arn
   starting_position = "LATEST"
   batch_size        = 10
 
-  # Filter for TTL deletions only (userIdentity is set by DynamoDB for TTL deletes)
   filter_criteria {
     filter {
       pattern = jsonencode({
